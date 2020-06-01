2ae0d45ee77e4faba4879a9ecaef406ae3f4f7d2
jnoman <jamalnoman62@gmail.com>
2020-06-01 19:54:01 +0100

    add mycommande

--- code/php/commandesStandard.php
+++ code/php/commandesStandard.php
@@ -9,19 +9,17 @@
     die("Connection failed: " . $conn->connect_error);
     }
 
-    $sql = "SELECT commande.idCommande,users.nom,users.prenom,commande.adresse,commande.telephone,commande.dateCommande FROM commande,users WHERE commande.etat_commande='en attente' AND commande.is_standard=1 AND commande.idUser=users.idUser ORDER BY commande.dateCommande ASC";
+    $sql = "SELECT commande.idCommande,users.nom,users.prenom,commande.dateCommande FROM commande,users WHERE commande.etat_commande='en attente' AND commande.is_standard=1 AND commande.idUser=users.idUser ORDER BY commande.dateCommande ASC";
     $result = $conn->query($sql);
 
     if ($result->num_rows > 0) {
         // output data of each row
-        echo '<table class="table table-striped divstandard">
+        echo '<div class="divstandard"><table class="table table-striped">
             <thead>
             <tr>
                 <th scope="col">id commande</th>
                 <th scope="col">nom client</th>
                 <th scope="col">prenom client</th>
-                <th scope="col">adresse</th>
-                <th scope="col">numéro de téléphone</th>
                 <th scope="col">date commande</th>
                 <th scope="col">terminé</th>
                 <th scope="col">refusé</th>
@@ -34,8 +32,6 @@
                     <th scope="row" name="idproduit">'. $row["idCommande"].'</th>
                     <td>'. $row["nom"].'</td>
                     <td>'. $row["prenom"].'</td>
-                    <td>'. $row["adresse"].'</td>
-                    <td>'. $row["telephone"].'</td>
                     <td>'. $row["dateCommande"].'</td>
                     <td><button type="submit" class="btn btn-light btn-lg" name="termine['. $row["idCommande"].']">terminé</button></td>
                     <td><button type="submit" class="btn btn-light btn-lg" name="refuse['. $row["idCommande"].']">refusé</button></td>
@@ -43,10 +39,10 @@
                 </tr>';
         }
         echo "</tbody>
-        </table>";
+        </table></div>";
     } 
     else {
-    echo '<p class="text-center font-weight-bolder">Aucun catégorie</p>';
+        echo '<div class="divstandard"><p class="text-center font-weight-bolder">Aucun commande</p></div>';
     }
     $conn->close();
     include('footer.php');
